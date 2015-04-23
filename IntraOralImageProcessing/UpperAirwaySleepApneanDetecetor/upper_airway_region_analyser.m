00b756a6bc216c2ca6c392233da999b8bcc5197e
Esuabom David Dijemeni <e.dijemeni@imperial.ac.uk>
2015-04-23 11:49:30 +0100

    Started developing an algoirthm to just detect the upper airway and draw a rectangle around it

---
+++ IntraOralImageProcessing/UpperAirwaySleepApneanDetecetor/upper_airway_region_analyser.m
@@ -0,0 +1,8 @@
+% Name: Esuabom David Dijemeni
+% Date: 23/04/2015
+% Purpose: Detect the airways using simple algorithm
+
+
+%% Read test image into matlab
+test_image = imread('C:\Users\Dijemni\Documents\GitHub\MedTechDevelopers\IntraOralImageProcessing\UpperAirwaySleepApneanDetecetor\Test_Image.jpg');
+figure(1); imagesc(test_image)
