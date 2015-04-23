7ae7d5e2cf6f1c27b8b48200b3c7174ebd3b4c32
Esuabom David Dijemeni <e.dijemeni@imperial.ac.uk>
2015-04-23 12:03:20 +0100

    Drawn a rectangle around a region in an image

--- IntraOralImageProcessing/UpperAirwaySleepApneanDetecetor/draw_a_shape_around_image_region.m
+++ IntraOralImageProcessing/UpperAirwaySleepApneanDetecetor/draw_a_shape_around_image_region.m
@@ -4,3 +4,13 @@
 
 %% Create the shape inserter
 shapeInserter = vision.ShapeInserter;
+
+%%  Read the input image
+I = imread('cameraman.tif');
+
+%% Define the rectangle dimensions as [x y width height]
+rectangle = int32([10 10 30 30]); %The point the the top left corner of the rectangle
+
+%% Draw the rectangle and display the result
+J = step(shapeInserter, I, rectangle);
+imshow(J);
